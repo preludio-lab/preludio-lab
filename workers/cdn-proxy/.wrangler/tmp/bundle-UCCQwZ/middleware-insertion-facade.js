b72379ee373e4bb390eb0f57444cbd5aeb5e3ba5
PreludioLab Admin <admin@preludiolab.com>
2026-01-14 13:51:17 +0900

    fix(git): 不要な.wranglerファイルをリポジトリから削除

--- workers/cdn-proxy/.wrangler/tmp/bundle-UCCQwZ/middleware-insertion-facade.js
+++
@@ -1,7 +0,0 @@
-import worker, * as OTHER_EXPORTS from '/Users/tetsu/work/preludio-lab/workers/cdn-proxy/src/index.ts';
-import * as __MIDDLEWARE_0__ from '/Users/tetsu/work/preludio-lab/workers/cdn-proxy/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts';
-
-export * from '/Users/tetsu/work/preludio-lab/workers/cdn-proxy/src/index.ts';
-
-export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [__MIDDLEWARE_0__.default];
-export default worker;
