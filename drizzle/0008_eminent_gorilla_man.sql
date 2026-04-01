de23bad47795f0dcc2f714240b04816028254bab
PreludioLab Admin <admin@preludiolab.com>
2026-04-01 11:15:00 +0900

    temp: revert local migration to pull from master

--- drizzle/0008_eminent_gorilla_man.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `work_part_translations` ADD `title_components` text;--> statement-breakpoint
-ALTER TABLE `work_parts` ADD `title_components` text;--> statement-breakpoint
-ALTER TABLE `work_parts` ADD `full_title` text;--> statement-breakpoint
-ALTER TABLE `work_translations` ADD `title_components` text;--> statement-breakpoint
-ALTER TABLE `works` ADD `title_components` text;--> statement-breakpoint
-ALTER TABLE `works` ADD `full_title` text;--> statement-breakpoint
-ALTER TABLE `works` ADD `search_text` text;
